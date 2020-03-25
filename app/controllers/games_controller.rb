b3b79f2b9a4c19e71f043557fec1562b2ba8da63
simon <sjtucker28@gmail.com>
2020-03-25 09:58:42 +0000

    backend as of Tuesday evening

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,25 @@
 class GamesController < ApplicationController
+
+    def start_a_game
+        g1 = Game.create()
+        question = QuoteGame.create(game: g1, quote: g1.new_quote)
+        render json: quesiton
+    end
+
+    def submit_a_question
+        question = Question.find()
+        question.update()
+        if g1.finished
+            redirect_to 
+        else
+            question = QuoteGame.create(...)
+            render json: question
+        end
+    end
+
+    def end_a_game
+        g1 = Game.find
+
+        render json: Game.leaderboard
+    end
 end
