15e041161fe67e42a7a2c13381e5ba855f7158d1
Rik280491 <57149887+Rik280491@users.noreply.github.com>
2020-03-25 10:13:36 +0000

    few extras added. nested authors json (#2)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,6 +7,12 @@ class AuthorsController < ApplicationController
 
     def show 
         author = Author.find(params[:id])
-        render json: author
+        render json: author, include: [:quotes]
     end
+
+
+
+
+
+
 end
