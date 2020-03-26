253d7f54e7d6b3bbc326fe42d841c6396977f483
RikBala <rik280491@googlemail.com>
2020-03-26 12:17:00 +0000

    new API, fix username

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 
     def start_game 
         game = Game.new 
-        random_quotes = Quote.all.sample(10)
+        random_quotes = Quote.all.sample(1)
         quote_author = random_quotes.map{|q| q.author}.uniq
         # random_quote_content = random_quotes.each{|q| q.content} 
         random_author = Author.all.sample
@@ -12,6 +12,31 @@ class GamesController < ApplicationController
         
     end 
 
+    def get_rounds
+        random_quotes = Quote.all.sample(10)
+        quote_author = random_quotes.map{|q| q.author}.uniq
+        # random_quote_content = random_quotes.each{|q| q.content} 
+        random_author = Author.all.sample
+        
+        render json: random_quotes.map { |quote|
+            {
+                quote: quote.content,
+                author: quote.author,
+                other_author: Author.random_except(quote.author)
+            }
+        }
+    end
 
+    def create
+        game = Game.create(game_params)
+
+        render json: game
+    end
+
+    private
+
+    def game_params
+        params.permit(:username, :score, quote_ids: [])
+    end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ApplicationRecord
 has_many :quotes 
 has_many :author_tags 
 has_many :tags, through: :author_tags
+
+    def self.random_except(author)
+        (self.all - [author]).sample
+    end
 end
