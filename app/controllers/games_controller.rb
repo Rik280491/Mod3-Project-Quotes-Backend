8cc000d3dafeb4b12730d92ceda3a755c8dc6fbc
simon <sjtucker28@gmail.com>
2020-03-26 10:54:51 +0000

    Slick Rick getting is panties in a knot

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,29 +1,5 @@
 class GamesController < ApplicationController
 
-<<<<<<< HEAD
-    def start_a_game
-        g1 = Game.create()
-        question = QuoteGame.create(game: g1, quote: g1.new_quote)
-        render json: quesiton
-    end
-
-    def submit_a_question
-        question = Question.find()
-        question.update()
-        if g1.finished
-            redirect_to 
-        else
-            question = QuoteGame.create(...)
-            render json: question
-        end
-    end
-
-    def end_a_game
-        g1 = Game.find
-
-        render json: Game.leaderboard
-    end
-=======
 
     def start_game 
         game = Game.new 
@@ -35,8 +11,4 @@ class GamesController < ApplicationController
         render json: [ quotes: random_quotes, authors: quote_author ]
         
     end 
-
-
-
->>>>>>> f8b494c2ace25d99c9df26819b79b9e41cc4b253
 end
